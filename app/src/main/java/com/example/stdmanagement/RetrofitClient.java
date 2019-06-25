a6746f450dbeca323dbac7fc21bb2e358b8071b8
oaojjj <oxojjjj@gmail.com>
2019-06-25 16:14:55 +0900

    회원가입(retrofit), db중복처리

---
+++ app/src/main/java/com/example/stdmanagement/RetrofitClient.java
@@ -0,0 +1,27 @@
+package com.example.stdmanagement;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private  static final String BASE_URL = "http://192.168.0.15/";
+    private  static RetrofitClient mInstance;
+    private Retrofit retrofit;
+
+    private RetrofitClient(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance(){
+        if(mInstance == null){
+            mInstance = new RetrofitClient();
+        }
+        return mInstance;
+    }
+    public RetroApi getApi(){
+        return retrofit.create(RetroApi.class);
+    }
+}
