80360e1344f63e2c89c9ffe2f34b78f3f3daeb4d
OseongKwon <oxojjjj@gmail.com>
2019-07-05 20:17:23 +0900

    Update RetrofitClient.java

--- app/src/main/java/com/example/stdmanagement/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/stdmanagement/Retrofit/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private  static final String BASE_URL = "http://211.215.75.134/";
+    private  static final String BASE_URL = "http://my ip/";
     private  static RetrofitClient mInstance;
     private Retrofit retrofit;
 
